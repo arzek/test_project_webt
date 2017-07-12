15082b002e4a50d0e04fc00c6878e8676c6934ca
Arzek <arzek2015@gmail.com>
2017-07-12 16:44:31 +0300

    database: Migrations table `currencies`

--- database/migrations/2017_07_12_092152_create_table_currencies.php
+++ database/migrations/2017_07_12_092152_create_table_currencies.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateTableCurrencies extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,14 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('currencies', function (Blueprint $table)
+        {
+
+            $table->integer('r030');
+            $table->string('txt');
+            $table->string('rate');
+            $table->string('cc');
+            $table->dateTime('exchangedate');
         });
     }
 
@@ -27,6 +31,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        //
     }
 }
